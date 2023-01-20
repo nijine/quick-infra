78f1cf4ccd6a90f0eec0faf5a434696b2d259a5c
Dmitriy Loshakov <winterdrift@gmail.com>
2023-01-20 00:06:59 +0000

    Implement runBaseCmd for all existing commands, add quiet params

--- entrypoint.py
+++ entrypoint.py
@@ -3,23 +3,32 @@
 import argparse
 import subprocess
 import sys
-from threading import Thread
 
 
-def runBaseCmd(cmd: list, opts: dict={}) -> None:
+def runBaseCmd(cmd: list, quiet: bool=False) -> None:
+    errs = []
+
     with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as t_proc:
-        for line in t_proc.stdout:
-            print(line.rstrip())
+        if not quiet:
+            for line in t_proc.stdout:
+                print(line.rstrip())
+
+        for line in t_proc.stderr:
+            errs.append(line.rstrip())
 
     if t_proc.returncode != 0:
-        print("Issue running {cmd}")
+        for line in errs:
+            print(line)
+        print(f"Issue running command: {' '.join(cmd)}")
         sys.exit(1)
 
 
 def runCreate(args: argparse.Namespace) -> None:
     # parse options
     # verbosity - there's absolutely a better way to do this but this works for now
-    quiet = args.quiet
+    quiet_init = args.quiet >= 1
+    quiet_plan = args.quiet >= 2
+    quiet_apply = args.quiet >= 3
 
     # terraform working dir
     infra_root = args.type
@@ -32,22 +41,23 @@ def runCreate(args: argparse.Namespace) -> None:
     # run terraform init, workspace opts if not n/a, and plan/apply
     init_args = ["terraform", f"-chdir={root_dir}", "init"]
 
-    runBaseCmd(init_args)
+    runBaseCmd(init_args, quiet_init)
 
     # run plan
     plan_args = ["terraform", f"-chdir={root_dir}", "plan"]
 
-    runBaseCmd(plan_args)
+    runBaseCmd(plan_args, quiet_plan)
 
     # run apply
     apply_args = ["terraform", f"-chdir={root_dir}", "apply", "-auto-approve"]
 
-    runBaseCmd(apply_args)
+    runBaseCmd(apply_args, quiet_apply)
 
 
 def runPreview(args: argparse.Namespace) -> None:
     # parse options
-    quiet = args.quiet
+    quiet_init = args.quiet >= 1
+    quiet_plan = args.quiet >= 2  # in case you just want to see that no errors occur
 
     # terraform working dir
     infra_root = args.type
@@ -60,16 +70,19 @@ def runPreview(args: argparse.Namespace) -> None:
     # run terraform init, workspace opts if not n/a, and plan
     init_args = ["terraform", f"-chdir={root_dir}", "init"]
 
-    runBaseCmd(init_args)
+    runBaseCmd(init_args, quiet_init)
 
     # run plan
     plan_args = ["terraform", f"-chdir={root_dir}", "plan"]
 
-    runBaseCmd(plan_args)
+    runBaseCmd(plan_args, quiet_plan)
 
 
 def runDestroy(args: argparse.Namespace) -> None:
     # parse options
+    quiet_init = args.quiet >= 1
+    quiet_destroy = args.quiet >= 2
+
     infra_root = args.type
 
     # populate config
@@ -80,20 +93,12 @@ def runDestroy(args: argparse.Namespace) -> None:
     # run terraform init, workspace opts if not n/a, and plan/apply
     init_args = ["terraform", f"-chdir={root_dir}", "init"]
 
-    result = subprocess.run(init_args, capture_output=True)
-
-    if result.returncode != 0:
-        print(result.stdout, '\n', result.stderr)
-        raise Exception("Terraform init failed during runDestroy!")
+    runBaseCmd(init_args, quiet_init)
 
     # run destroy
     init_args = ["terraform", f"-chdir={root_dir}", "destroy", "-auto-approve"]
 
-    result = subprocess.run(init_args, capture_output=True)
-
-    if result.returncode != 0:
-        print(result.stdout, '\n', result.stderr)
-        raise Exception("Terraform init failed during runDestroy!")
+    runBaseCmd(init_args, quiet_destroy)
 
 
 def main() -> None:
@@ -102,12 +107,12 @@ def main() -> None:
     # maybe make the choices auto-poplate based on file dirs?
     parser.add_argument("type", help="The type of infra to create", choices=["site", "app"])
     parser.add_argument("-o", "--options", help="A comma-separated list of optional settings to use with --type")
-    parser.add_argument("-q", "--quiet", help="Reduce output to errors only (-q) or no output (-qq)", action="count", default=0)
+    parser.add_argument("-q", "--quiet", help="Reduces output (hides the various terraform run stages i.e. init, plan, apply)", action="count", default=0)
     parser.add_argument("-b", "--bucket", help="An S3 bucket to use for remote state")
     parser.add_argument("-s", "--state-name", help="A custom name to use for the state file")
     parser.add_argument("-w", "--workspace", help="A custom workspace name")
 
-    # TODO: Add some descriptors to the actions, if possible
+    # TODO: Add some descriptors to the actions, as needed
 
     args = parser.parse_args()
 
